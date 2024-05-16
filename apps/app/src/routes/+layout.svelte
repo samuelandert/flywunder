a05363508fe8a1e5c21dd3af7c86f236763f70cd
Samuel Andert <samuel@andert.me>
2024-05-16 15:45:15 +0200

    added more svelte boilderplate

--- apps/app/src/routes/+layout.svelte
+++ apps/app/src/routes/+layout.svelte
@@ -15,6 +15,15 @@
 		<ComposeView view={$drawerStore.meta} />
 	</Drawer>
 
+	<nav class="p-4 text-white bg-gray-800">
+		<ul class="flex space-x-4">
+			<li><a href="/" class="hover:text-gray-300">Home</a></li>
+			<li><a href="/countries" class="hover:text-gray-300">Countries</a></li>
+			<li><a href="/todos" class="hover:text-gray-300">Todos</a></li>
+			<li><a href="/edit" class="hover:text-gray-300">Edit</a></li>
+		</ul>
+	</nav>
+
 	<AppShell>
 		<slot />
 	</AppShell>
