02c860fb4dcc03441a6fa785f96d8a37065879e4
Samuel Andert <samuel@andert.me>
2024-05-18 12:21:55 +0200

    WIP: supabase auth

--- apps/app/src/routes/+layout.svelte
+++ apps/app/src/routes/+layout.svelte
@@ -3,11 +3,26 @@
 	import { Drawer, AppShell, initializeStores, getDrawerStore } from '@skeletonlabs/skeleton';
 	import { QueryClientProvider } from '@tanstack/svelte-query';
 	import type { LayoutData } from './$types';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
 
 	export let data: LayoutData;
 
 	initializeStores();
 	const drawerStore = getDrawerStore();
+
+	let { supabase, session } = data;
+	$: ({ supabase, session } = data);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
 <QueryClientProvider client={data.queryClient}>
@@ -20,6 +35,7 @@
 			<li><a href="/" class="hover:text-gray-300">Home</a></li>
 			<li><a href="/countries" class="hover:text-gray-300">Countries</a></li>
 			<li><a href="/edit" class="hover:text-gray-300">Edit</a></li>
+			<li><a href="/account" class="hover:text-gray-300">Account</a></li>
 		</ul>
 	</nav>
 
